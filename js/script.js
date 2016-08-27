29dbf5dc9d9e5514e716f74b57020e5707370e7d
Stefan-Cosmin Romanescu <stefanromanescu@Stefans-iMac.local>
2016-08-27 17:55:29 +0300

    Asynchronous home page

--- js/script.js
+++ js/script.js
@@ -3,62 +3,52 @@
  */
 $(document).ready(function () {
 
-    //initialize swiper when document ready
-
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        effect:'coverflow',
-        slidesPerView: 2,
-        centeredSlides: true,
-        grabCursor: true,
-        hashnav:true,
-        // If we need pagination
-        pagination: '.swiper-pagination',
-
-        // Navigation arrows
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-
-        // And if we need scrollbar
-        scrollbar: '.swiper-scrollbar',
-    });
     $("#navbarToggle").blur(function(event){
-        var screenWidth = window.innerWidth;
-        if(screenWidth < 768){
+        if(window.innerWidth < 768){
             $("#collapsable-nav").collapse('hide');
         }
     });
     /*
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();
-    });
-    overlay.click(function(){
-        hamburger_cross();
-    });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
+    function insertHTML(selector, htmlToInsert){
+        $(selector).html(htmlToInsert);
     }
+    $.get("snippets/home-snippet.html", function(data){
+        ins
+    });*/
+    (function (window){
+        var homeHTML="snippets/home-snippet.html";
+        function insertHTML(selector, htmlToInsert){
+            $(selector).html(htmlToInsert);
+        }
+        function showLoading (selector){
+            insertHTML(selector, "<div class='text-center'>" + "<img src='images/gears.svg'></div>");
+        }
+        showLoading("#mainContent");
+        $.get(homeHTML, function(data){
+            $("#mainContent").html(data);
+        }).done(function(){
+            var mySwiper = new Swiper ('.swiper-container', {
+                // Optional parameters
+                direction: 'horizontal',
+                loop: true,
+                effect:'coverflow',
+                slidesPerView: 2,
+                centeredSlides: true,
+                grabCursor: true,
+                hashnav:true,
+                // If we need pagination
+                pagination: '.swiper-pagination',
 
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-    });
-    */
+                // Navigation arrows
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+
+                // And if we need scrollbar
+                scrollbar: '.swiper-scrollbar',
+            });
+        });
+    })(window);
+
+    //initialize swiper when document ready
+
 });
