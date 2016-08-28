f97b5f0d847eec71fad575f9f8697ace8f898565
Stefan-Cosmin Romanescu <stefanromanescu@Stefans-iMac.local>
2016-08-28 23:50:39 +0300

    Added panouri radiante + termostat + json
    v0.5

--- js/script.js
+++ js/script.js
@@ -16,14 +16,29 @@ $(document).ready(function () {
         ins
     });*/
     (function (window){
+
+        var lema = {};
+
         var homeHTML="snippets/home-snippet.html";
+        var allProductsUrl = "panouriRadiante.json";
+        var productsTitleHtml = "snippets/products-title-snippet.html";
+        var productsHtml = "snippets/products-snippet.html";
+
         function insertHTML(selector, htmlToInsert){
             $(selector).html(htmlToInsert);
         }
+
         function showLoading (selector){
             insertHTML(selector, "<div class='text-center'>" + "<img src='images/gears.svg'></div>");
         }
+
+        //Return substitute of '{{propName}}' with propValue in given 'string'
+        function insertProperty (string, propName, propValue){
+            return string.replace(new RegExp("{{" + propName + "}}", "g"), propValue);
+        }
+
         showLoading("#mainContent");
+
         $.get(homeHTML, function(data){
             $("#mainContent").html(data);
         }).done(function(){
@@ -47,6 +62,35 @@ $(document).ready(function () {
                 scrollbar: '.swiper-scrollbar',
             });
         });
+
+        lema.loadProductsCategories = function (){
+            showLoading("#mainContent");
+            $.get(allProductsUrl, buildAndShowProductsHTML);
+        };
+
+        function buildAndShowProductsHTML(products){
+            $.get(productsTitleHtml, function(productsTitleHtml){
+                //Retrive single product snippet
+                $get(productsHtml, function(productsHtml){
+                    var productsViewHtml =
+                        bulidProductsViewHtml(products, productsTitleHtml, productsHtml);
+                    insertHTML("#mainContect", productsViewHtml);
+                })
+            })
+        }
+
+        function buildProductsViewHtml(products, productsTitleHtml, productsHtml){
+            var finalHtml = productsTitleHtml;
+            finalHtml += "<section class='row";
+            for (var i = 0; i < products.length; i++){
+                var html = productsHtml;
+                var name = "" + products[i].name;
+                html = insertProperty(html, "name", name);
+                finalhtml += html;
+            }
+        }
+
+        window.$lema = lema;
     })(window);
 
     //initialize swiper when document ready
